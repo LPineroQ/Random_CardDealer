8bed5b6f88a32d5713fdd9ccb85af836d3190948
Luis Piñero Quintana <lpineroq@gmail.com>
2022-02-17 18:54:20 +0000

    commit 2

--- src/app.js
+++ src/app.js
@@ -2,6 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  console.log("Everything is working!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suits = ["diamonds", "spades", "clubs", "hearts"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
 };
